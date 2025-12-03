fcd0e20608a6ddf00979e3d4d98b64fdbc867bc8
Wendulka <wendulka@example.com>
2025-12-03 20:12:48 +0100

    Configure CORS for hollyhop.cz

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  console.log("Backend starting...");
+  app.enableCors({
+    origin: [
+      'https://hollyhop.cz',
+      'http://hollyhop.cz',
+      'https://otamat.w33.wedos.net',
+      'http://localhost:3000',
+      'http://localhost:4000'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  console.log("Backend starting with CORS enabled for hollyhop.cz...");
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
